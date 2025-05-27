70f6bf1b0d99f195a900d8fd3024120469a8dbba
GabrielAKaufmann <gangelokaufmann@gmail.com>
2025-05-27 20:43:30 -0300

    first commit

---
+++ src/support/pages/PlaylistPage.ts
@@ -0,0 +1,24 @@
+import { Page } from '@playwright/test';
+import { PlaylistElements } from '../elements/PlaylistElements';
+
+export class PlaylistPage {
+  constructor(private page: Page) {}
+
+  async createPlaylistAndAddMusic() {
+    await this.page.click(PlaylistElements.buttonPlus);
+
+    await this.page.click(PlaylistElements.createPlaylistButton);
+
+    const musicName = `Do Fundo da Grota`;
+
+    await this.page.fill(PlaylistElements.placeHolderAddMusic, musicName);
+
+    const firstTrack = await this.page
+      .locator(PlaylistElements.tracklistLocator)
+      .first();
+
+    await firstTrack.locator(PlaylistElements.addTrack).click();
+
+    return musicName;
+  }
+}
